b3efceb4b51267d7fd6992d931447d4bf80c0b96
Jesús Santiago <systemsroad@gmail.com>
2020-09-11 08:18:20 -0300

    aggregate data for buttons

---
+++ src/data.js
@@ -0,0 +1,95 @@
+export const data = [
+    {
+        row: 1,
+        values: [
+            {
+                value: '+',
+                styles: 'btn' 
+            },
+            {
+                value: '-',
+                styles: 'btn' 
+            },
+            {
+                value: 'x',
+                styles: 'btn' 
+            },
+            {
+                value: '/',
+                styles: 'btn' 
+            }
+        ]
+    },
+    {
+        row: 2,
+        values: [
+            {
+                value: '7',
+                styles: 'btn' 
+            },
+            {
+                value: '8',
+                styles: 'btn' 
+            },
+            {
+                value: '9',
+                styles: 'btn' 
+            },
+            {
+                value: '=',
+                styles: 'btn btn-blue' 
+            }
+        ]
+    },
+    {
+        row: 3,
+        values: [
+            {
+                value: '4',
+                styles: 'btn' 
+            },
+            {
+                value: '5',
+                styles: 'btn' 
+            },
+            {
+                value: '6',
+                styles: 'btn' 
+            }
+        ]
+    },
+    {
+        row: 4,
+        values: [
+            {
+                value: '1',
+                styles: 'btn' 
+            },
+            {
+                value: '2',
+                styles: 'btn' 
+            },
+            {
+                value: '3',
+                styles: 'btn' 
+            }
+        ]
+    },
+    {
+        row: 5,
+        values: [
+            {
+                value: '0',
+                styles: 'btn' 
+            },
+            {
+                value: '.',
+                styles: 'btn' 
+            },
+            {
+                value: 'AC',
+                styles: 'btn btn-red' 
+            }
+        ]
+    }
+];
