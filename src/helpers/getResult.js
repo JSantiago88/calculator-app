9f542f20db08ace3200e4a5ac0b0881964b076b7
Jesús Santiago <systemsroad@gmail.com>
2020-09-26 18:44:55 -0300

    refactoring for getResult function

--- src/helpers/getResult.js
+++ src/helpers/getResult.js
@@ -1,28 +1,7 @@
 
 export const getResult = (initialValue = []) => {
 
-    let arr = initialValue.join('')
-        .replaceAll('.', '')
-        .replaceAll(",", ".");
-
-    let digits = [];
-    for (let i = 0, j = 0; i < arr.length; i++) {
-
-        if (isNaN(parseFloat(arr.charAt(i))) && arr.charAt(i) !== '.') {
-
-            digits.push(parseFloat(arr.slice(j, i)));
-            digits.push(arr.slice(i, i + 1));
-            j = i + 1;
-        } else if (i === arr.length - 1) {
-            digits.push(parseFloat(arr.slice(j, i + 1)));
-        }
-    }
-
-    if (isNaN(digits[digits.length - 1])) {
-        digits.pop();
-    }
-
-    const calculate = (digits) => {
+    const calculate = (digits = []) => {
         let stackOperators = [];
         let stackAux = [];
         let total = 0;
@@ -33,97 +12,137 @@ export const getResult = (initialValue = []) => {
 
             if (!isNaN(digits[i])) {
 
-                if (isNaN(digits[i + 1])) {
+                if (isNaN(digits[i + 1]) && digits[i + 1] !== undefined) {
                     result = 0;
-                    if (digits[i + 1] !== undefined) {
+                   
+                    if (stackOperators[stackOperators.length - 1] !== undefined) {
 
-                        if (stackOperators[stackOperators.length - 1] !== undefined) {
+                        const stOperator = stackOperators[stackOperators.length - 1];
 
-                            const stOperator = stackOperators[stackOperators.length - 1];
+                        if ((stOperator === '+' || stOperator === '-') && (digits[i + 1] === '+' || digits[i + 1] === '-')) {
 
-                            if ((stOperator === '+' || stOperator === '-') && (digits[i + 1] === '+' || digits[i + 1] === '-')) {
-
-                                if (stOperator === '+') { // step
-                                    result = total + digits[i];
-                                } else {
-                                    result = total - digits[i];
-                                }
-
-                                flag = false;
-                                stackOperators.pop();
+                            if (stOperator === '+') {
+                                result = total + digits[i];
                             } else {
-
-                                if (stOperator === 'x' || stOperator === '/') {// step
-
-                                    if (stOperator === 'x') {
-                                        result = total * digits[i];
-                                    } else {
-                                        result = total / digits[i];
-                                    }
-
-                                    flag = false;
-                                    stackOperators.pop();
-                                } else {
-                                    stackAux.push(total);
-                                }
+                                result = total - digits[i];
                             }
-                        }
 
-                        stackOperators.push(digits[i + 1]);
+                            flag = false;
+                            stackOperators.pop();
+                        } else if (stOperator === 'x' || stOperator === '/') {
 
-                        if (flag) { // step
-                            total = digits[i];
+                            if (stOperator === 'x') {
+                                result = total * digits[i];
+                            } else {
+                                result = total / digits[i];
+                            }
+
+                            flag = false;
+                            stackOperators.pop();
                         } else {
-                            total = result;
+                            stackAux.push(total);
                         }
+                    }
 
-                        i++;
-                        if (digits.length > i) {
-                            continue;
-                        }
+                    stackOperators.push(digits[i + 1]);
+
+                    if (flag) {
+                        total = digits[i];
                     } else {
-
-                        if (digits[i - 1] === '+') {
-                            total += digits[i];
-                        } else if (digits[i - 1] === '-') {
-                            total -= digits[i];
-                        } else if (digits[i - 1] === 'x') {
-                            total *= digits[i];
-                        } else if (digits[i - 1] === '/') {
-                            total /= digits[i];
-                        }
-                        stackOperators.pop();
+                        total = result;
                     }
 
-                    let valueOperator;
-                    let valueNumber;
-                    while (stackAux.length > 0) { // step
-                        valueOperator = stackOperators[stackOperators.length - 1];
-                        valueNumber = stackAux[stackAux.length - 1];
-
-                        if (valueOperator === '+') {
-                            total = valueNumber + total;
-                        } else if (valueOperator === '-') {
-                            total = valueNumber - total;
-                        } else if (valueOperator === 'x') {
-                            total = valueNumber * total;
-                        } else if (valueOperator === '/') {
-                            total = valueNumber / total;
-                        }
-
-                        stackOperators.pop()
-                        stackAux.pop()
+                    i++;
+                    if (digits.length > i) {
+                        continue;
                     }
+                    
+                } else {
+
+                    total = digits[i - 1] === '+' ? total + digits[i] :
+                            digits[i - 1] === '-' ? total - digits[i] :
+                            digits[i - 1] === 'x' ? total * digits[i] :
+                            digits[i - 1] === '/' ? total / digits[i] :
+                            total;
+
+                    stackOperators.pop();
                 }
+
+                total = execPendingOperations(stackAux, stackOperators, total);
             }
         }
 
         return [
-            total.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, ".").includes('e') ?
-                'error' :
-                total.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+            amountFormat(total)
         ];
-    }
+    };
+
+    const joinDigitsToString = (initialValue) => {
+        return initialValue.join('')
+            .replaceAll('.', '')
+            .replaceAll(",", ".");
+    };
+
+    const formatChainDigitsToArr = (chainDigits) => {
+        const result = [];
+        for (let i = 0, j = 0; i < chainDigits.length; i++) {
+
+            if (isNaN(parseFloat(chainDigits.charAt(i))) && chainDigits.charAt(i) !== '.') {
+
+                result.push(parseFloat(chainDigits.slice(j, i)));
+                result.push(chainDigits.slice(i, i + 1));
+                j = i + 1;
+            } else if (i === chainDigits.length - 1) {
+                result.push(parseFloat(chainDigits.slice(j, i + 1)));
+            }
+        }
+        return result;
+    };
+    
+    const cleanLastOperator = (digits) => {
+        if (isNaN(digits[digits.length - 1])) {
+            digits.pop();
+        }
+        return digits;
+    };
+
+    const execPendingOperations = (stackAux = [], stackOperators, total) => {
+        let valueOperator;
+        let valueNumber;
+
+        while (stackAux.length > 0) {
+            valueOperator = stackOperators[stackOperators.length - 1];
+            valueNumber = stackAux[stackAux.length - 1];
+
+            total = valueOperator === '+' ? valueNumber + total :
+                    valueOperator === '-' ? valueNumber - total :
+                    valueOperator === 'x' ? valueNumber * total :
+                    valueOperator === '/' ? valueNumber / total :
+                    total;
+
+            stackOperators.pop()
+            stackAux.pop()
+        }
+        return total;
+    };
+
+    const amountFormat = (total = 0) => {
+
+        const rExpresion = /\B(?=(\d{3})+(?!\d))/g;
+
+        const _amountFormat = (total) => {
+            return total.toFixed(2)
+                .replace('.', ',')
+                .replace(rExpresion, ".");
+        };
+
+        return _amountFormat(total).includes('e') ? 'error' : _amountFormat(total);
+    };
+
+    const digits = cleanLastOperator(
+        formatChainDigitsToArr(
+            joinDigitsToString(initialValue)
+        ));
 
     return calculate(digits);
 };
